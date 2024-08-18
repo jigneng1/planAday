b10085b712d03ef11a44e1e532f23b2eb59d280d
Neng Sitiporn <jigneng1@gmail.com>
2024-08-18 18:10:49 +0700

    add : menu

---
+++ lib/src/app.dart
@@ -0,0 +1,61 @@
+// Layout widget
+
+import 'package:flutter/material.dart';
+import 'package:plan_a_day/src/screens/home_screen.dart';
+import 'package:plan_a_day/src/screens/profile_screen.dart';
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _currentIndex = 0;
+
+  // put the screen widget here
+  final List<Widget> _children = [HomeScreen(), ProfileScreen()];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        margin: const EdgeInsets.only(top: 10),
+        height: 64,
+        width: 64,
+        child: FloatingActionButton(
+          backgroundColor: Colors.orange[900],
+          elevation: 0,
+          onPressed: () => debugPrint("Add"),
+          shape: RoundedRectangleBorder(
+              side: const BorderSide(width: 3, color: Colors.transparent),
+              borderRadius: BorderRadius.circular(100)),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
