16a9f944beea46b500a86ecbfb145ab9d584b3ba
Kanyarat <pigsweet1412@gmail.com>
2024-08-24 14:32:32 +0700

    add : create plan page

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PlanAday',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           useMaterial3: true,
           primaryColor: Colors.orange,
