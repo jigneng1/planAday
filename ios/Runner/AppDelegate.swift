97de8b4f233a8ce8e79bb06fffaa46c40e029bd2
Neng Sitiporn <jigneng1@gmail.com>
2024-08-31 21:11:15 +0700

    config : ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,33 +1,33 @@
+import UIKit
+import Flutter
+import GoogleMaps
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDm4ccjZLWgT4hMrRIFwvHw2twI0Uw0rBI")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+// AppDelegate.swift
 // import UIKit
-// import Flutter
 // import GoogleMaps
 
 // @UIApplicationMain
-// @objc class AppDelegate: FlutterAppDelegate {
-//   override func application(
-//     _ application: UIApplication,
-//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//   ) -> Bool {
-//     GMSServices.provideAPIKey("AIzaSyDm4ccjZLWgT4hMrRIFwvHw2twI0Uw0rBI")
-//     GeneratedPluginRegistrant.register(with: self)
-//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//   }
+// class AppDelegate: UIResponder, UIApplicationDelegate {
+
+//     var window: UIWindow?
+
+//     func application(_ application: UIApplication,
+//                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//         // Provide the API key
+//         GMSServices.provideAPIKey("API_key")
+//         return true
+//     }
 // }
-
-// AppDelegate.swift
-import UIKit
-import GoogleMaps
-
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Provide the API key
-        GMSServices.provideAPIKey("API_key")
-        return true
-    }
-}
 
