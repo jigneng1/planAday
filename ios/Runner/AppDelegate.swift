875c0f252cdea077437f120d89563ab637bcc0c4
Kanyarat <pigsweet1412@gmail.com>
2024-08-31 17:05:09 +0700

    add : Google Map Set API Key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,33 @@
-import Flutter
-import UIKit
+// import UIKit
+// import Flutter
+// import GoogleMaps
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+// @UIApplicationMain
+// @objc class AppDelegate: FlutterAppDelegate {
+//   override func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//   ) -> Bool {
+//     GMSServices.provideAPIKey("AIzaSyDm4ccjZLWgT4hMrRIFwvHw2twI0Uw0rBI")
+//     GeneratedPluginRegistrant.register(with: self)
+//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+// }
+
+// AppDelegate.swift
+import UIKit
+import GoogleMaps
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Provide the API key
+        GMSServices.provideAPIKey("API_key")
+        return true
+    }
 }
+
