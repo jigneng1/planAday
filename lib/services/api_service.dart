9861698d25d9e9bf1a1fa0dfe179121fff54385f
Kanyarat <pigsweet1412@gmail.com>
2024-09-17 13:21:55 +0700

    fix : receive data from API

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,19 +3,22 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   // Function to send planData to the API and return the random places data
-  Future<Map<String, dynamic>?> sendJsonData(Map<String, dynamic> planData) async {
-    final url = Uri.parse('http://localhost:3000/nearby-search'); // Replace with your API endpoint
+  Future<Map<String, dynamic>?> sendJsonData(
+      Map<String, dynamic> planData) async {
+    final url = Uri.parse(
+        'http://localhost:3000/nearby-search'); // Replace with your API endpoint
     // final url = Uri.parse('http://10.0.2.2:3000/nearby-search');
 
     try {
       // Send the planData to the API
       final response = await http.post(
         url,
-        body: {
-          "lad" : planData['lad'],
-          "lng" : planData['lng'],
-          "category" : planData['categories'].toString(),
-        }, 
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          "lad": planData['lad'],
+          "lng": planData['lng'],
+          "category": ['restaurant'],
+        }),
       );
 
       if (response.statusCode == 200) {
@@ -23,24 +26,26 @@ class ApiService {
 
         // Parse the response body to extract the plan ID
         final responseData = jsonDecode(response.body);
-        final String planID = responseData['id']; // Assuming 'id' is the field in the response
+        final String planID =
+            responseData['id']; // Assuming 'id' is the field in the response
 
-        if (planID != null) {
-          final numberOfPlace = planData['numberOfPlace'];
-          final placesUrl = Uri.parse("http://localhost:3000/randomPlaces?id=$planID&places=$numberOfPlace");
-          final placesResponse = await http.get(placesUrl);
+        final numberOfPlace = planData['numberOfPlaces'];
+        final placesUrl = Uri.parse(
+            "http://localhost:3000/randomPlaces?id=$planID&places=$numberOfPlace");
+        final placesResponse = await http.get(placesUrl);
 
-          if (placesResponse.statusCode == 200) {
-            print('Random places fetched successfully');
-            // Parse and return the places data
-            final planData = jsonDecode(placesResponse.body); 
-            return planData['data'];
-          } else {
-            print('Failed to fetch random places: ${placesResponse.statusCode}');
-            return null;
-          }
+        if (placesResponse.statusCode == 200) {
+          print('Random places fetched successfully');
+          // Parse and return the places data
+          final planData = jsonDecode(placesResponse.body);
+          final List<dynamic> places = planData['data'];
+          final Map<String, dynamic> placesMap = {
+            for (var place in places) place['id']: place
+          };
+          print('Random places data: $placesMap');
+          return ;
         } else {
-          print('No planID found in the response');
+          print('Failed to fetch random places: ${placesResponse.statusCode}');
           return null;
         }
       } else {
