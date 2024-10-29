2864c4a453cd8a4468a85d4b26a8661fb7421d1e
t-thanakorn.cho <thanakornc91@gmail.com>
2024-10-29 14:45:35 +0700

    [UPDATE] WelcomeScreen Center

--- lib/src/screens/welcome_screen.dart
+++ lib/src/screens/welcome_screen.dart
@@ -21,24 +21,20 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   void initState() {
     super.initState();
 
-    // Initialize the animation controller
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 2),
     );
 
-    // Fade-in animation
     _fadeInAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
     );
 
-    // Slide-up animation for the text
     _slideUpAnimation =
         Tween<Offset>(begin: const Offset(0, 0.2), end: Offset.zero).animate(
       CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
     );
 
-    // Start the animations when the screen loads
     _animationController.forward();
   }
 
@@ -48,7 +44,6 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     super.dispose();
   }
 
-  // Fixed token check function
   Future<bool> _checkTokenExistsAndExpire() async {
     try {
       String? token = await getToken();
@@ -60,36 +55,29 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   }
 
   void _handleAuthCheck(BuildContext context) async {
-    // Show loading indicator
     showDialog(
       context: context,
       barrierDismissible: false,
-      builder: (BuildContext context) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
+      builder: (BuildContext context) => const LoadingIndicator(),
     );
 
     try {
       bool isAuthenticated = await _checkTokenExistsAndExpire();
+      Navigator.pop(context); // Close loading indicator
 
       if (isAuthenticated) {
-        // Navigate to home or dashboard
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const MainLayout()),
         );
       } else {
-        // User is not authenticated
-        // Navigate to login page
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const LoginScreen()),
         );
       }
     } catch (e) {
-      // Show error message
+      Navigator.pop(context);
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text('Error: ${e.toString()}'),
@@ -103,8 +91,6 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        height: double.infinity,
-        width: double.infinity,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color(0xfffb9a4b), Color(0xffff6838)],
@@ -113,94 +99,104 @@ class _WelcomeScreenState extends State<WelcomeScreen>
             end: Alignment.bottomRight,
           ),
         ),
-        child: Column(
-          children: [
-            // Logo with fade-in animation
-            FadeTransition(
-              opacity: _fadeInAnimation,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 120.0),
-                child: Image.asset(
-                  'assets/images/Asset_14x.png',
-                  height: 150,
-                ),
-              ),
-            ),
-            const SizedBox(height: 100),
-            // Sliding-up Text animations
-            SlideTransition(
-              position: _slideUpAnimation,
-              child: const Column(
-                children: [
-                  Text(
-                    'Welcome to PlanADay',
-                    style: TextStyle(
-                      fontSize: 28,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 12),
-                  Text(
-                    'Suggest trip in one day',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 30),
-            GestureDetector(
-              onTap: () => _handleAuthCheck(context),
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 200),
-                height: 53,
-                width: 320,
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.white),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Get Started',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+        child: Center( // Center content vertically and horizontally
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Center content within the screen height
+            children: [
+              FadeTransition(
+                opacity: _fadeInAnimation,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Image.asset(
+                    'assets/images/Asset_14x.png',
+                    height: 150,
                   ),
                 ),
               ),
-            ),
-            const Spacer()
-          ],
+              const SizedBox(height: 50),
+              SlideTransition(
+                position: _slideUpAnimation,
+                child: const WelcomeText(),
+              ),
+              const SizedBox(height: 30),
+              GetStartedButton(onTap: () => _handleAuthCheck(context)),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-void main() async {
-  await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class WelcomeText extends StatelessWidget {
+  const WelcomeText({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login App',
-      theme: ThemeData(
-        useMaterial3: true,
-        primaryColor: const Color(0xFFFF6838),
-        primarySwatch: Colors.orange,
-      ),
-      home: const WelcomeScreen(),
+    return const Column(
+      children: [
+        Text(
+          'Welcome to PlanADay',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 12),
+        Text(
+          'Suggest trip in one day',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
+
+class GetStartedButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const GetStartedButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        height: 53,
+        width: 320,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(color: Colors.white),
+        ),
+        child: const Center(
+          child: Text(
+            'Get Started',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
+
